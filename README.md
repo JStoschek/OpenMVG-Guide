cdaa9870f30972db92cc4eeabef2193eef5c22cd
JStoschek <jonathan.stoschek@gmail.com>
2024-05-29 16:23:50 -0700

    Completed README and fixed python Script

--- README.md
+++ README.md
@@ -2,3 +2,36 @@
 
 OpenMVG (Open Multiple View Geometry) is an open-source library designed for photogrammetry and computer vision, focusing on Structure from Motion (SfM) and 3D reconstruction. Written in C++, it supports feature extraction, matching, robust estimation, and camera modeling to build accurate 3D models from 2D images. OpenMVG offers both incremental and global SfM pipelines, integrates with tools like OpenMVS for dense reconstruction, and supports various export formats for visualization and further processing.
 
+# Instructions 
+
+Required tools:
+ 
+ - Git
+ - Docker
+
+Clone repository into folder and build the docker image:
+```shell
+$ git clone https://github.com/JStoschek/OpenMVG-Guide.git
+$ cd OpenMVG-Guide/
+$ git clone --recursive https://github.com/openMVG/openMVG.git
+$ cd openMVG/
+$ docker build . -t openmvg
+```
+
+Run docker container:
+```shell
+$ docker run -it --rm -v /Path/To/OpenMVG-Guide:/files openmvg
+EXAMPLE: $ docker run -it --rm -v /Users/jstoschek/Documents/GitHub/OpenMVG-Guide:/files openmvg
+```
+
+Run Python Script:
+```shell
+$ cd files/
+$ python3 run_OpenMVG_SfM.py
+```
+
+
+
+
+
+
--- run_OpenMVG_SfM.py
+++ run_OpenMVG_SfM.py
@@ -2,88 +2,98 @@ import os
 import subprocess
 import sys
 
+# Set the directory paths
+input_dir = '/files/data/imagesBookshelf'  # Directory containing the input images
+output_dir = '/files/data'  # Directory for storing the script's output
+camera_file_params = '/files/data/sensor_width_camera_database.txt'  # Path to the camera sensor width database file
 
-input_dir = '/files/data/imagesBookshelf' # The directory of the images to to use 
-output_dir = '/files/data' # The directory of the script output
-camera_file_params = '/files/openMVG/src/openMVG/exif/sensor_width_database/sensor_width_camera_database.txt' # The diectory of the database file with sensor widths
-
+# Define the directories for matches and reconstruction
 matches_dir = os.path.join(output_dir, 'matches')
 reconstruction_dir = os.path.join(output_dir, 'reconstruction')
 
+# Create directories if they do not exist
 if not os.path.exists(output_dir):
-  os.mkdir(output_dir)
+    os.mkdir(output_dir)
 if not os.path.exists(matches_dir):
-  os.mkdir(matches_dir)
+    os.mkdir(matches_dir)
 if not os.path.exists(reconstruction_dir):
-  os.mkdir(reconstruction_dir)
+    os.mkdir(reconstruction_dir)
 
-print ('1. Intrinsics analysis')
+# Step 1: Intrinsics analysis
+print('1. Intrinsics analysis')
 command = [
-    'openMVG_main_SfMInit_ImageListing',
-    '-i', input_dir,
-    '-o', matches_dir,
-    '-d', camera_file_params
+    'openMVG_main_SfMInit_ImageListing',  # Command for image listing
+    '-i', input_dir,  # Input directory
+    '-o', matches_dir,  # Output directory for matches
+    '-d', camera_file_params  # Camera database file
 ]
 result = subprocess.run(command, text=True, check=True)
 
-print ('\n2. Compute features')
+# Step 2: Compute features
+print('\n2. Compute features')
 command = [
-    'openMVG_main_ComputeFeatures',
-    '-i', matches_dir+'/sfm_data.json', 
-    '-o', matches_dir, 
-    '-m', 'SIFT'
+    'openMVG_main_ComputeFeatures',  # Command for computing features
+    '-i', matches_dir + '/sfm_data.json',  # Input data
+    '-o', matches_dir,  # Output directory
+    '-m', 'SIFT'  # Feature extraction method
 ]
 result = subprocess.run(command, text=True, check=True)
 
-print ("\n3. Compute matching pairs")
+# Step 3: Compute matching pairs
+print('\n3. Compute matching pairs')
 command = [
-    'openMVG_main_PairGenerator',
-    "-i", matches_dir+"/sfm_data.json", 
-    "-o" , matches_dir + "/pairs.bin"
+    'openMVG_main_PairGenerator',  # Command for generating pairs
+    '-i', matches_dir + '/sfm_data.json',  # Input data
+    '-o', matches_dir + '/pairs.bin'  # Output pairs file
 ]
 result = subprocess.run(command, text=True, check=True)
 
-print ("\n4. Compute matches")
+# Step 4: Compute matches
+print('\n4. Compute matches')
 command = [
-    'openMVG_main_ComputeMatches',
-    "-i", matches_dir+"/sfm_data.json", 
-    "-p", matches_dir+ "/pairs.bin", 
-    "-o", matches_dir + "/matches.putative.bin"
+    'openMVG_main_ComputeMatches',  # Command for computing matches
+    '-i', matches_dir + '/sfm_data.json',  # Input data
+    '-p', matches_dir + '/pairs.bin',  # Input pairs file
+    '-o', matches_dir + '/matches.putative.bin'  # Output matches file
 ]
 result = subprocess.run(command, text=True, check=True)
 
-print ("\n5. Filter matches" )
+# Step 5: Filter matches
+print('\n5. Filter matches')
 command = [
-    'openMVG_main_GeometricFilter',
-    "-i", matches_dir+"/sfm_data.json", 
-    "-m", matches_dir+"/matches.putative.bin" , 
-    "-g" , "f" , 
-    "-o" , matches_dir+"/matches.f.bin"
+    'openMVG_main_GeometricFilter',  # Command for filtering matches
+    '-i', matches_dir + '/sfm_data.json',  # Input data
+    '-m', matches_dir + '/matches.putative.bin',  # Input matches file
+    '-g', 'f',  # Geometric filter type
+    '-o', matches_dir + '/matches.f.bin'  # Output filtered matches file
 ]
 result = subprocess.run(command, text=True, check=True)
 
-print ("\n6. Do Sequential/Incremental reconstruction")
+# Step 6: Sequential/Incremental reconstruction
+print('\n6. Do Sequential/Incremental reconstruction')
 command = [
-    'openMVG_main_SfM',
-    "--sfm_engine", "INCREMENTAL", 
-    "-i", matches_dir+"/sfm_data.json", 
-    "-m", matches_dir, 
-    "-o", reconstruction_dir
+    'openMVG_main_SfM',  # Command for Structure from Motion (SfM)
+    '--sfm_engine', 'INCREMENTAL',  # SfM engine type
+    '-i', matches_dir + '/sfm_data.json',  # Input data
+    '-m', matches_dir,  # Input matches directory
+    '-o', reconstruction_dir  # Output reconstruction directory
 ]
 result = subprocess.run(command, text=True, check=True)
 
-print ("\n7. Colorize Structure")
+# Step 7: Colorize Structure
+print('\n7. Colorize Structure')
 command = [
-    'openMVG_main_ComputeSfM_DataColor',
-    "-i", reconstruction_dir+"/sfm_data.bin", 
-    "-o", reconstruction_dir+"/colorized.ply"
+    'openMVG_main_ComputeSfM_DataColor',  # Command for colorizing the structure
+    '-i', reconstruction_dir + '/sfm_data.bin',  # Input data
+    '-o', reconstruction_dir + '/colorized.ply'  # Output colorized file
 ]
 result = subprocess.run(command, text=True, check=True)
 
-print ("\n8. Store the data as JSON file")
+# Step 8: Store the data as JSON file
+print('\n8. Store the data as JSON file')
 command = [
-    'openMVG_main_ConvertSfM_DataFormat',
-    "-i", reconstruction_dir+"/sfm_data.bin", 
-    "-o", reconstruction_dir+"/sfm_data.json"
+    'openMVG_main_ConvertSfM_DataFormat',  # Command for converting data format
+    '-i', reconstruction_dir + '/sfm_data.bin',  # Input data
+    '-o', reconstruction_dir + '/sfm_data.json'  # Output JSON file
 ]
 result = subprocess.run(command, text=True, check=True)
